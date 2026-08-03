4fa3d5e0611feceb976faf8c138a27ce0012d886
danielzera777-debug <danielzera777@gmail.com>
2026-08-03 16:25:38 -0300

    Create     app2.py

---
+++ app2.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Robô Ofertas ML está online!"
+
+@app.route("/oferta")
+def oferta():
+    return jsonify({
+        "produto": "Oferta de teste",
+        "preco": "R$ 99,90",
+        "desconto": "30%",
+        "link": "https://mercadolivre.com.br"
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
