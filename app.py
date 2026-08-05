02fb5d61b1c8139c9634ba8cf118b309256bdb4b
danielzera777-debug <danielzera777@gmail.com>
2026-08-05 06:06:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -665,20 +665,13 @@ def me():
 
 
 if __name__ == "__main__":
-if __name__ == "__main__":
-
 
     app.run(
-
         host="0.0.0.0",
-
         port=int(
-
             os.getenv(
                 "PORT",
                 5000
             )
-
         )
-
     )
